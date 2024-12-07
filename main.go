3f8af0d1b1c23d7c23f422071a843511ecaac4b4
Marcus Adams <8154210+adamsma@users.noreply.github.com>
2024-12-06 23:46:39 -0500

    initial file server and health checkpoint

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 
-	sMux := http.NewServeMux()
+	const filepathRoot = "."
+	const port = "8080"
 
-	server := http.Server{
+	sMux := http.NewServeMux()
+	sMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	sMux.HandleFunc("/healthz", handlerHealth)
+
+	server := &http.Server{
 		Handler: sMux,
-		Addr:    ":8080",
+		Addr:    ":" + port,
 	}
 
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+
+}
+
+func handlerHealth(resp http.ResponseWriter, req *http.Request) {
+
+	resp.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	resp.WriteHeader(http.StatusOK)
+
+	resp.Write([]byte(http.StatusText(http.StatusOK)))
 
 }
